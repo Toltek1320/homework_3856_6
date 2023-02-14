347375ce245e2187f1f3b42e787d9523d3ab6cef
Ruslan <123066002+Toltek1320@users.noreply.github.com>
2023-02-14 20:45:52 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ for (int i = 0; i < numbers.Length; i++)
         sum++;
     }
 }
-// Console.WriteLine();
 Console.Write($"-> {sum}");
 int[] StringToNum(string input)
 {
@@ -50,12 +49,44 @@ int[] StringToNum(string input)
     return numbers;
 }
 void PrintArray(int[] array)
-{
-    // Console.Write("[ ");
+{    
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write(array[i] + " ");
     }
-    // Console.Write("]");
 }
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Две прямые пересекутся в точке с координатами: X = {x}; Y = {y}");
+
+// Задача 44:выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 --> 0 1 1 2 3
+// Если N = 3 --> 0 1 1
+// Если N = 7 --> 0 1 1 2 3 5 8
+
+Console.Write("Введите значение n: ");
+double n = Convert.ToInt32(Console.ReadLine());
+double f(int n)
+{
+    if(n == 0) return 0;
+    if(n == 1 || n == 2) return 1;
+    else return f(n - 1) + f(n - 2);
+}
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"f({i + 1}) = {f(i)}");
+}
